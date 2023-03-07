db15c879f035402176a56692bc094c0b774d58a2
Chris Snowden <chrissnowden35@gmail.com>
2023-03-07 15:49:45 +1100

    Keep track of guesses

--- src/components/Wordle/grid/GamePanel.tsx
+++ src/components/Wordle/grid/GamePanel.tsx
@@ -20,9 +20,12 @@ export const GamePanel = () => {
 
   const [gameState, setGameState] = useState<gameState>('Not Started');
 
+  const [previousGuesses, setPreviousGuesses] = useState<string[][]>([]);
+  const [currentGuess, setCurrentGuess] = useState<string[]>([])
+
   const [isRevealing, setIsRevealing] = useState(false)
+
   const [frameBatch, setFrameBatch] = useState<string[]>([]);
-  const [prediction, setPrediction] = useState<string | undefined>();
 
   const onFrameCapture = (frame: string) => {
     setFrameBatch(prevFrames => [...prevFrames.slice(prevFrames.length - 20), frame])
@@ -33,10 +36,13 @@ export const GamePanel = () => {
     setTimeout(() => {
       setIsRevealing(false)
     }, 350 * 5)
+
+    setPreviousGuesses(previousGuesses => [...previousGuesses, currentGuess])
+    setCurrentGuess([])
   }
 
   const handlePredictionResponse = (prediction: string) => {
-    setPrediction(prediction)
+    setCurrentGuess(currentGuess => [...currentGuess, prediction.toUpperCase()])
   }
 
   const handleGameStart = () => {
@@ -53,9 +59,6 @@ export const GamePanel = () => {
          });
   }
 
-
-  console.log(gameState);
-
   return (
     <Grid container alignItems="center"  direction="row" justifyContent="center">
     <Grid item xs={6}>
@@ -66,12 +69,11 @@ export const GamePanel = () => {
     <Grid item xs={6}>
       <div className="mx-auto flex w-full grow flex-col px-1 pt-2 pb-8 sm:px-6 md:max-w-7xl lg:px-8 short:pb-2 short:pt-2">
         <div className="flex grow flex-col justify-center pb-6 short:pb-2">
-          <WordleGrid solution='apple' currentGuess={['A']} guesses={guesses} isRevealing={isRevealing} numberOfAttempts={6} />
+          <WordleGrid solution='apple' currentGuess={currentGuess} guesses={previousGuesses} isRevealing={isRevealing} numberOfAttempts={6} />
         </div>
       </div>
     </Grid>
     <Grid item xs={6}>
-      <div>Prediction {prediction}</div>
       <Button variant="contained" onClick={handleGameStart}>Start</Button>
       <Button variant="contained" onClick={() => handleSubmitLetterFrames(frameBatch)}>Submit Letter</Button>
       <Button variant="contained" onClick={handleTryGuess}>Try Guess</Button>
