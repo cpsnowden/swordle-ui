b7d75ec398a7065977fc0232f3b3c0bf7f79e6a5
Chris Snowden <chrissnowden35@gmail.com>
2023-03-15 21:42:57 +1100

    Try resizing webcam

--- src/components/WebcamContainer/WebcamContainer.tsx
+++ src/components/WebcamContainer/WebcamContainer.tsx
@@ -2,6 +2,18 @@ import { forwardRef } from "react";
 import Webcam from "react-webcam";
 import { videoConstraints } from "services/params";
 
-export const WebcamContainer = forwardRef<Webcam>((_, ref) => (
-  <Webcam audio={false} videoConstraints={videoConstraints} ref={ref} />
-));
+export const WebcamContainer = forwardRef<Webcam>((_, ref) => {
+  const width = 200;
+  return (
+    <Webcam
+      audio={false}
+      style={{
+        borderRadius: `30px 30px 30px 30px`,
+      }}
+      videoConstraints={videoConstraints}
+      width={width}
+      height={(width * 9) / 16}
+      ref={ref}
+    />
+  );
+});
